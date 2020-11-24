81586fc705271e92705063f8989ea6c5c9240f4d
Yu Gao <gaoyububest@gmail.com>
2020-11-24 20:11:59 +0000

    order by modified

--- 4_queries/13_name_of_teachers_and_number_of_assistances.sql
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -1,8 +1,8 @@
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, COUNT(assistance_requests.*) AS total_assistances
+SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) AS total_assistances
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
 GROUP BY teachers.name, cohorts.name
-ORDER BY total_assistances;
+ORDER BY total_assistances DESC;
